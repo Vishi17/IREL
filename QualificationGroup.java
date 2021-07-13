2e086a4ea18009296972f0aaebef378156ebb085
Vishi17 <87085586+Vishi17@users.noreply.github.com>
2021-07-13 22:26:00 +0530

    Update QualificationGroup.java
    Updated

--- QualificationGroup.java
+++ QualificationGroup.java
@@ -12,10 +12,14 @@ public class QualificationGroup {
 
 	public static void main(String[] args) throws Throwable {
 		QualificationGroup login = new QualificationGroup();
-//		login.login();
-//		login.QualificationGroups();
-		login.qgCreate();
-		
+		login.login();
+		login.QualificationGroups();
+		login.qgFill();
+		login.qgBack();
+		login.qgErroralert();
+		login.showEntries();
+		login.edit();
+		login.editField();
 
 	}
 	//Testcase for Login - HR- master - main- logout
@@ -64,7 +68,7 @@ public class QualificationGroup {
 	  	driver.quit();
 	  } 
 	//testcase- Qualification Groups - create new-Yes- save-logout
-	public void qgCreate() throws InterruptedException {
+	public void qgFill() throws InterruptedException {
 		System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver(); 
 		
@@ -82,18 +86,152 @@ public class QualificationGroup {
 		driver.findElement(By.xpath("//a[contains(text(),'Add More Groups')]")).click();
 		driver.findElement(By.xpath("//input[@name='groupCode']")).sendKeys("July12");
 		driver.findElement(By.xpath("//div[contains(text(),'Select some options')]")).click();
-		Thread.sleep(3000);
-		//driver.findElement(By.xpath("//div[@id='content-main-section']")).sendKeys("BBM");
-		WebElement qualification = driver.findElement(By.xpath("//div[@id='content-main-section']"));
+		//driver.findElement(By.xpath("//input[@id='content-main-section']")).sendKeys("BBM");
+		WebElement qualification = driver.findElement(By.xpath("//div[contains(text(),'BBM')]"));
+		qualification.click();
 		Thread.sleep(5000);
-		qualification.sendKeys("BBM");
-		driver.findElement(By.xpath("//input[@class='content-main-section']")).click();
-		driver.findElement(By.xpath("//input[@type='reset']")).click();
-		driver.findElement(By.xpath("//a[contains(text(),'Go Back')]")).click();
+		driver.findElement(By.xpath("//div[@class='fs-label-wrap']")).click();
+		driver.findElement(By.xpath("//input[@type='submit']")).click();
 		driver.findElement(By.xpath("//a[contains(text(),'Go Back')]")).click();
 		driver.findElement(By.xpath("//a[text()='Logout']")).click();
 	    driver.quit();
 	}
+		//Testcase - Create new -back to qualificatioType - Go back to 
+		public void qgBack() {
+			System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+			WebDriver driver =new ChromeDriver(); 
+			
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+			driver.get("https://115.124.105.99:6443/humanres/control/main");
+			driver.findElement(By.xpath("//button[contains(text(),'Advanced')]")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Proceed to 115.124.105.99 (unsafe)')]")).click();
+			driver.findElement(By.xpath("//input[@name='USERNAME']")).sendKeys("cmstest");
+			driver.findElement(By.xpath("//input[@name='PASSWORD']")).sendKeys("ofbiz321");
+			driver.findElement(By.xpath("//input[@type='submit']")).click(); 
+		    driver.findElement(By.xpath("//h2[text()='Human Resources']")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Master')]")).click();
+			driver.findElement(By.xpath("//a[contains(text(),' Qualification Groups')]")).click();
+			//driver.findElement(By.xpath("//a[contains(text(),'Add More Groups')]")).click();
+			//Go Back 
+			driver.findElement(By.xpath("//a[contains(text(),'Go Back')]")).click();
+			driver.findElement(By.xpath("//a[text()='Logout']")).click();
+			driver.quit();
+		}
+		//Testcase - handling error pop up
+		public void qgErroralert() throws InterruptedException {
+			System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			
+			driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+			driver.get("https://115.124.105.99:6443/humanres/control/main");
+			driver.findElement(By.xpath("//button[contains(text(),'Advanced')]")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Proceed to 115.124.105.99 (unsafe)')]")).click();
+			//Thread.sleep(7000);
+			driver.findElement(By.xpath("//input[@name='USERNAME']")).sendKeys("cmstest");
+			driver.findElement(By.xpath("//input[@name='PASSWORD']")).sendKeys("ofbiz321");
+			driver.findElement(By.xpath("//input[@type='submit']")).click(); 
+		    driver.findElement(By.xpath("//h2[text()='Human Resources']")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Master')]")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Qualification Groups')]")).click();
+			//create New
+			driver.findElement(By.xpath("//a[contains(text(),'Add More Groups')]")).click();
+			driver.findElement(By.xpath("//input[@name='groupCode']")).sendKeys("July12");
+			driver.findElement(By.xpath("//div[contains(text(),'Select some options')]")).click();
+			//driver.findElement(By.xpath("//input[@id='content-main-section']")).sendKeys("BBM");
+			WebElement qualification = driver.findElement(By.xpath("//div[contains(text(),'BBM')]"));
+			qualification.click();
+			Thread.sleep(5000);
+			driver.findElement(By.xpath("//div[@class='fs-label-wrap']")).click();
+			driver.findElement(By.xpath("//input[@type='submit']")).click();
+			driver.findElement(By.xpath("//button[@class='jGrowl-close']")).click();
+			driver.findElement(By.xpath("//a[contains(text(),'Go Back')]")).click();
+			driver.findElement(By.xpath("//a[text()='Logout']")).click();
+		    driver.quit();
+		}
+		//Testcase for show entries and search 
+				public void showEntries() throws InterruptedException {
+					System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+					WebDriver driver = new ChromeDriver();
+					
+					driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+					driver.get("https://115.124.105.99:6443/humanres/control/main");
+					driver.findElement(By.xpath("//button[contains(text(),'Advanced')]")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Proceed to 115.124.105.99 (unsafe)')]")).click();
+
+					driver.findElement(By.xpath("//input[@name='USERNAME']")).sendKeys("cmstest");
+					driver.findElement(By.xpath("//input[@name='PASSWORD']")).sendKeys("ofbiz321");
+					driver.findElement(By.xpath("//input[@type='submit']")).click(); 
+				    driver.findElement(By.xpath("//h2[text()='Human Resources']")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Master')]")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Qualification Groups')]")).click();
+					Select se = new Select(driver.findElement(By.xpath("//*[@name='DataTables_Table_0_length']")));
+					se.selectByValue("10");
+					Thread.sleep(5000);
+					driver.findElement(By.xpath("//input[@type='search']")).sendKeys("MCOM");
+					driver.findElement(By.xpath("//a[text()='Logout']")).click();
+				    driver.quit();
+				}
+				//Testcase for list of  master - Edit button
+				public void edit() throws InterruptedException {
+					System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+					WebDriver driver = new ChromeDriver();
+					driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+					driver.get("https://115.124.105.99:6443/humanres/control/main");
+					driver.findElement(By.xpath("//button[contains(text(),'Advanced')]")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Proceed to 115.124.105.99 (unsafe)')]")).click();
+
+					//Thread.sleep(7000);
+					driver.findElement(By.xpath("//input[@name='USERNAME']")).sendKeys("cmstest");
+					driver.findElement(By.xpath("//input[@name='PASSWORD']")).sendKeys("ofbiz321");
+					driver.findElement(By.xpath("//input[@type='submit']")).click(); 
+				    driver.findElement(By.xpath("//h2[text()='Human Resources']")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Master')]")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Qualification Groups')]")).click();
+					//edit button
+					driver.findElement(By.xpath("(//a[text()='Edit'][1])")).click();
+					//driver.findElement(By.xpath("(//td[text()='Edit'][2])")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Go Back')]")).click();
+					driver.findElement(By.xpath("//a[text()='Logout']")).click();
+					driver.quit();
+				}
+				//Testcase - edit the data 
+				public void editField() throws InterruptedException {
+					System.setProperty("webdriver.chrome.driver", "C:\\Program Files\\chromedriver.exe");
+					WebDriver driver = new ChromeDriver();
+					driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+					driver.get("https://115.124.105.99:6443/humanres/control/main");
+					driver.findElement(By.xpath("//button[contains(text(),'Advanced')]")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Proceed to 115.124.105.99 (unsafe)')]")).click();
+					
+					driver.findElement(By.xpath("//input[@name='USERNAME']")).sendKeys("cmstest");
+					driver.findElement(By.xpath("//input[@name='PASSWORD']")).sendKeys("ofbiz321");
+					driver.findElement(By.xpath("//input[@type='submit']")).click(); 
+				    driver.findElement(By.xpath("//h2[text()='Human Resources']")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Master')]")).click();
+					driver.findElement(By.xpath("//a[contains(text(),' Qualification Groups')]")).click();
+			
+					//edit button
+					driver.findElement(By.xpath("(//a[text()='Edit'][1])")).click();
+					//edit fields
+					WebElement groupcde = driver.findElement(By.xpath("//input[@name='groupCode']"));
+					groupcde.clear();
+					groupcde.sendKeys("July");
+					driver.findElement(By.xpath("//div[@class='fs-label-wrap']")).click();
+					//driver.findElement(By.xpath("//input[@id='content-main-section']")).sendKeys("BBM");
+					WebElement qualification = driver.findElement(By.xpath("//div[contains(text(),'BBM')]"));
+					qualification.click();
+					WebElement qualification1 = driver.findElement(By.xpath("//div[contains(text(),'MSC')]"));
+					Thread.sleep(3000);
+					qualification1.click();
+					WebElement checkbox=driver.findElement(By.xpath("//div[@class='fs-label-wrap']"));
+					checkbox.click();
+					driver.findElement(By.xpath("//input[@type='submit']")).click();
+					driver.findElement(By.xpath("//button[@class='jGrowl-close']")).click();
+					driver.findElement(By.xpath("//a[contains(text(),'Go Back')]")).click();
+					driver.findElement(By.xpath("//a[text()='Logout']")).click();
+					driver.quit();	
+				}
 	
 
 }
+
